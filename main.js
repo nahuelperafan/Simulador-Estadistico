736275a019a4bdd33cc65d3221ec72e3cfeca894
nahuelperafan <nahuelperafan@gmail.com>
2023-01-26 18:10:19 -0300

    Se agregaron botones adicionales y se agrego funcionalidad del paginado

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         language: {
             // Se setean los valores iniciales
             "lengthMenu": "Mostrar _MENU_ registros",
-            "zeroRecords": "No se encontraron resultados",
+            // "zeroRecords": "No se encontraron resultados",
             // "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
             // "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
             // "infoFiltered": "(filtrado de un total de _MAX_ registros)",
@@ -14,7 +14,7 @@ $(document).ready(function() {
             //     "sNext":"Siguiente",
             //     "sPrevious": "Anterior"
             //  },
-             "sProcessing":"Procesando...",
+            //  "sProcessing":"Procesando...",
         },
         //para usar los botones   
         responsive: "false",
