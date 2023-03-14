615be2c73a4baff35ce1ef2a70c7c22b75bf6f34
nahuelperafan <nahuelperafan@gmail.com>
2023-03-14 11:36:34 -0300

    Progreso intermedio.

--- main.js
+++ main.js
@@ -21,13 +21,73 @@ $(document).ready(function() {
       searching: false,
       //dom: 'Btl',       
       // buttons:[ 
-  // 	{
-  // 		// extend:    'excelHtml5',
-  // 		text:      '<i class="fas fa-trash-alt"></i> ',
-  // 		titleAttr: 'Limpiar datos',
-  // 		className: 'btn btn-success'
-  // 	},
+  //  {
+  //    // extend:    'excelHtml5',
+  //    text:      '<i class="fas fa-trash-alt"></i> ',
+  //    titleAttr: 'Limpiar datos',
+  //    className: 'btn btn-success'
+  //  },
 
-  // ]	        
+  // ]          
   });     
+});
+
+var miBoton = document.getElementById("miBoton");
+miBoton.addEventListener("click", function() {
+
+  // aquí va la función que se ejecutará cuando se haga clic en el botón
+  function sumarValores() {
+    var input = document.getElementById('miInput').value;
+    var tabla = $('#example').DataTable();
+    var celda = tabla.cell(0, 1);
+    var valorActual = parseInt(celda.data());
+    var resultado = valorActual + parseInt(input);
+    celda.data(resultado).draw();
+}
+  sumarValores();
+});
+
+var miBoton2 = document.getElementById("miBoton2");
+miBoton.addEventListener("click", function() {
+
+  // aquí va la función que se ejecutará cuando se haga clic en el botón
+  function restarValores() {
+    var input = document.getElementById('miInput2').value;
+    var tabla = $('#example').DataTable();
+    var celda = tabla.cell(0, 2);
+    var valorActual = parseInt(celda.data());
+    var resultado = valorActual - parseInt(input);
+    celda.data(resultado).draw();
+}
+  restarValores();
+})
+
+var miBoton3 = document.getElementById("miBoton3");
+miBoton.addEventListener("click", function() {
+
+  // aquí va la función que se ejecutará cuando se haga clic en el botón
+  function multiplicarValores() {
+    var input = document.getElementById('miInput3').value;
+    var tabla = $('#example').DataTable();
+    var celda = tabla.cell(0, 3);
+    var valorActual = parseInt(celda.data());
+    var resultado = valorActual * parseInt(input);
+    celda.data(resultado).draw();
+}
+multiplicarValores();
+});
+
+var miBoton4 = document.getElementById("miBoton4");
+miBoton.addEventListener("click", function() {
+
+  // aquí va la función que se ejecutará cuando se haga clic en el botón
+  function dividirValores() {
+    var input = document.getElementById('miInput4').value;
+    var tabla = $('#example').DataTable();
+    var celda = tabla.cell(0, 4);
+    var valorActual = parseInt(celda.data());
+    var resultado = valorActual % parseInt(input);
+    celda.data(resultado).draw();
+}
+  dividirValores();
 });
